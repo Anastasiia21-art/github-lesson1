52b97f72f27f9e0eea120e474e9e90eb7e7a9672
“Anastasiia” <kan6433@ya.ru>
2024-04-18 15:31:20 +0300

    Change react

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!!!!!пше ыефе
+          Learn React!!!
         </a>
       </header>
     </div>
