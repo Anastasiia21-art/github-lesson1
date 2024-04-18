23c47ee61aecc00fa5e7937460060e777f19ecf7
“Anastasiia” <kan6433@ya.ru>
2024-04-18 13:57:09 +0300

    reactgit statusgit status

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!!!!пше ыефе
         </a>
       </header>
     </div>
